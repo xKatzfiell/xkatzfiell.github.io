000a5973c76b0309f45bebe88fcaf1e48133ec52
Katzfiell <canyilmaz045@gmail.com>
2023-01-13 19:35:02 +0300

    .

---
+++ js/commands.js
@@ -0,0 +1,101 @@
+var password = "katz3131";
+var instagram = "https://instagram.com/mcanylmz.png/";
+var github = "https://github.com/xKatzfiell";
+
+whois = [
+  "<br>",
+  "Hey, I'm Katzfiell!👋",
+  "I am 17 years old.",
+  "I am trying to improve myself in software programming. Coding is a piece of cake. 😊",
+  "My Aim: Senior Developer.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram.com/mcanylmz.png/' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github.com/xKatzfiell' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+donation = [
+  "<br>",
+  '<span class="command">Hey thanks for thinking about donation 💖</span>',
+  '<a href="" target="_blank"Soon Paypal</a>',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Most projects are private on my Github.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Katz?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">video</span>          View YouTube videos',
+  '<span class="command">donate</span>         Donation Link',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+
+banner = [
+  "                                        :..:::^^:^^::^:.::                                             ",
+  "                                    ::^^^^^^^^^^^^^^^^^^^^^^^:^:                                    ",
+  "                                :^^^^:::::^^^^^^^^^^^^^^^^^^^^^^^^^:                                ",
+  "                            ::^^::..::^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^::                            ",
+  "                          :^^::..::^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^:                          ",
+  "                        ^^::..::^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^~^^                        ",
+  "                      ^^:...:^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^~~^                      ",
+  "                    :^^:..:^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^~~:                    ",
+  "                   :^:.:^~~~~~~~~~~~~^^^^^^^^^^^^^^^^^^^^^^^^^^~~~~~~~~~~~~~~^~~^                   ",
+  "                 ^7Y55PPGGBBBBBBBBGGGPP5YJ7!~^^^^^^^^^^~!7JY5PPGGGBBBBBBBGGGGPP5Y?^                 ",
+  "                 G######BGGGPPPPPPPGGGGB###BG5J?7777?J5GBBBBGGGGGPPPGBBBBB########G                 ",
+  "                :!JBBBBG5555555555555PGBBBBB####BPPB###BPPP55555555PBBBBBBBBBBBBBJ7^                ",
+  "               :^^^7BBBG5PPPPPPP55PPBBBBBBBBBB#Y!~~!Y#BG55PPPPP55PGBBBBBBBBBBBBB7~~~:               ",
+  "               :^^^^Y#BG5PPPP55PPGBBBBBBBBBBB#G~^^^^~P#BPPPP55PPGBBBBBBBBBBBBB#Y^~~!^               ",
+  "               ^^^^^7BBBP555PPGBBBBBBBBBBBBBB#J^^^^^^J#BP555PGBBBBBBBBBBBBBBBBB7^~~!~               ",
+  "               ^^^^^^5#BGPGGBBBBBBBBBBBBBBBB#P~^^^^^^~P#BGGBBBBBBBBBBBBBBBBBB#5^^~~~!:              ",
+  "              :^^^^^^~P##BBBBBBBBBBBBBBBBB##P!^^^^^^^^!P##BBBBBBBBBBBBBBBBB##P~^^~~~!:              ",
+  "              :^^^^^^^~JGB##BBBBBBBBBB###BGJ~^^^^^^^^^^~JGB###BBBBBBBBBB##BGJ~^^^~~~!:              ",
+  "               ^^^^^^^^^~?YPGBBB###BBBGPY7~^^^^^^^^^^^^^^~7YPGBBB###BBBGPY?~^^^^~~~~~               ",
+  "               ^^^^^^^^^^^^~~!7????77!~~^^^^^^^^^^^^^^^^^^^^~~!77????7!~~^^^^^^^~!~!~               ",
+  "               :^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^~~~~!:               ",
+  "                ^^^^^^^^^^^^^^^^^^~^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^~^^^^^^^^^^^^~~~~!^                ",
+  "                 ^^^^^^^^^^^^^^^^?G5?!~^^^^^^^^^^^^^^^^^^^^^^~!?5G?^^^^^^^^^^~~~~!~                 ",
+  "                  ^^^^^^^^^^^^^^^~Y##BG5J7!~^^^^^^^^^^^^~!7J5GB##Y~^^^^^^^^^~~~~!~:                 ",
+  "                   ^~^^^^^^^^^^^^^^!5B###BBGP55YYJJYY55PGBB###B5!^^^^^^^^^~~~~~!~                   ",
+  "                    :^^^^^^^^^^^^^^^^!JPB##################BPJ!^^^^^^^^^~~~~~!~^                    ",
+  "                      ^^^^^^^^^^^^^^^^^^!?Y5GGBBBBBBBBGG5Y?!^^^^^^^^^^~~~!~!!~:                     ",
+  "                       :^~^^^^^^^^^^^^^^^^^^~~!!7777!!~~^^^^^^^^^^^^~~~!~!!~:                       ",
+  "                         :^~~^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^~~~~!!!~~:                         ",
+  "                           :^~~~~^^^^^^^^^^^^^^^^^^^^^^^^^^^~~~~~~!!!~~^:                           ",
+  "                              :^^~~~~~~~^^^^^^^^^^^^^~~~~~~~~!!!!!~^^:                              ",
+  "                                  :^^~~~~~~~~~~~~~~~~~!!!!!!~~~^^:                                  ",
+  "                                      :::^~^~~~~~~~~~~~~^:^                                         ",
+
+
+    '<span class="color2">Welcome to my interactive web terminal.</span>',
+    "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+  ]
